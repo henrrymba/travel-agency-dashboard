f82d8f6eaa6e2de1cd939788dea3a294627900c1
Henrry Muraña Bautista <henrrymba@icloud.com>
2025-11-18 18:44:25 +0100

    feat(auth): implement authentication and UI using Appwrite

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,31 +1,34 @@
-import {Link, NavLink} from "react-router";
+import {Link, NavLink, useLoaderData, useNavigate} from "react-router";
 import {sidebarItems} from "~/constants";
 import {cn} from "~/lib/utils";
+import {logoutUser} from "~/appwrite/auth";
 
-const NavItems = ({ handleClick }: {handleClick?: () => void }) => {
-    const user = {
-        name: 'Adrian',
-        email: 'henrry@gmail.com',
-        imageUrl: '/assets/images/david.webp'
+const NavItems = ({ handleClick }: { handleClick?: () => void}) => {
+    const user = useLoaderData();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('/sign-in')
     }
 
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
-                <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]"/>
+                <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
                 <h1>Tourvisto</h1>
             </Link>
 
             <div className="container">
                 <nav>
-                    {sidebarItems.map(({ id, href, icon, label}) => (
+                    {sidebarItems.map(({ id, href, icon, label }) => (
                         <NavLink to={href} key={id}>
                             {({ isActive }: { isActive: boolean }) => (
                                 <div className={cn('group nav-item', {
                                     'bg-primary-100 !text-white': isActive
                                 })} onClick={handleClick}>
                                     <img
-                                        src="icon"
+                                        src={icon}
                                         alt={label}
                                         className={`group-hover:brightness-0 size-0 group-hover:invert ${isActive ? 'brightness-0 invert' : 'text-dark-200'}`}
                                     />
@@ -37,22 +40,21 @@ const NavItems = ({ handleClick }: {handleClick?: () => void }) => {
                 </nav>
 
                 <footer className="nav-footer">
-                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'Henrry'} />
+                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'Henrry'} referrerPolicy="no-referrer" />
 
                     <article>
                         <h2>{user?.name}</h2>
                         <p>{user?.email}</p>
                     </article>
-                    
-                    <button onClick={() => {
-                        console.log('logout')
-                    }}
-                    className="cursor-pointer"
+
+                    <button
+                        onClick={handleLogout}
+                        className="cursor-pointer"
                     >
                         <img
                             src="/assets/icons/logout.svg"
                             alt="logout"
-                            className="size-6 "
+                            className="size-6"
                         />
                     </button>
                 </footer>
@@ -60,4 +62,5 @@ const NavItems = ({ handleClick }: {handleClick?: () => void }) => {
         </section>
     )
 }
+
 export default NavItems
