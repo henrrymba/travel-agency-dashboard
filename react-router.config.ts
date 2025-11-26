d168e76a4e0028786c05670c7732e9fdd7633551
Henrry Muraña Bautista <henrrymba@icloud.com>
2025-11-26 19:25:17 +0100

    chore(deploy): optimize Vercel deployment configuration

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "@react-router/dev/config";
 import { sentryOnBuildEnd } from "@sentry/react-router";
+import { vercelPreset } from "@vercel/react-router/vite";
+
 export default {
   ssr: true,
+  presets: [vercelPreset()],
   buildEnd: async ({ viteConfig, reactRouterConfig, buildManifest }) => {
     // ...
     // Call this at the end of the hook
-    +(await sentryOnBuildEnd({ viteConfig, reactRouterConfig, buildManifest }));
+    await sentryOnBuildEnd({ viteConfig, reactRouterConfig, buildManifest });
   },
 } satisfies Config;
